05cdfd31cd0351789e579b1dbacf4ca84b0685a9
KM <18610929200@163.com>
2018-06-21 15:25:25 +0800

    window.rootVC

--- FuckComic/Classes/AppDelegate.swift
+++ FuckComic/Classes/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = UIColor.white
-//        window?.rootViewController = UITabBarController()
+        window?.rootViewController = UITabBarController()
         window?.makeKeyAndVisible()
         
         return true
